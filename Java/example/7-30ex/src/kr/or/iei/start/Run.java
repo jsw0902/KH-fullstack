fc259ea0e9277f324937027f7e703bf643bc2b68
jsw0902 <134297585+jsw0902@users.noreply.github.com>
2024-07-31 08:37:29 +0900

    Add files via upload

---
+++ Java/example/7-30ex/src/kr/or/iei/start/Run.java
@@ -0,0 +1,12 @@
+package kr.or.iei.start;
+import kr.or.iei.vo.Information; //실행 클래스
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Information info = new Information();
+		info.printinfo();
+	}
+
+}
---
+++ Java/example/7-30ex/src/kr/or/iei/vo/Information.java
@@ -0,0 +1,10 @@
+package kr.or.iei.vo;
+//기능 메소드
+public class Information {
+	public void printinfo() {
+		System.out.println("이름: 장승원");
+		System.out.println("나이: 23");
+		System.out.println("생년월일: 2002년 9월 2일");
+	}
+
+}
