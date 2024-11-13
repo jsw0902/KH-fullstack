dc98ccfc90184bcfa28472aa8b377582203de6e7
jsw0902 <134297585+jsw0902@users.noreply.github.com>
2024-11-13 12:12:00 +0900

    Add files via upload

---
+++ react/05_router_1113/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+  라우터 사용 시, App 컴포넌트 감싸는 태그 변경
+*/
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
